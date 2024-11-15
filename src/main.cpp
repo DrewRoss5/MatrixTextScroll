b8ae9cda4034adcf5a8a37ec1b2bab6dacf75221
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-11-14 20:45:12 -0700

    Implement the remaining command-line argument

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,11 @@ int main(int argc, char** argv){
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "displays the help menu")
-        ("minlen", po::value<int>(), "the minimum length for each vertical line of text (default = 10)")
-        ("maxlen", po::value<int>(), "the maximum length for each vertical line of text (default = 15)")
+        ("minlen",   po::value<int>(), "the minimum length for each vertical line of text (default = 10)")
+        ("maxlen",   po::value<int>(), "the maximum length for each vertical line of text (default = 15)")
+        ("delay",   po::value<int>(), "the delay in milliseconds between each row being printed (default = 30)")
+        ("color",    po::value<std::string>(), "the color for the lines to be displayed in. Valid options are: red, green, yellow, blue, magenta, cyan, grey, and white (default = green)")
+        ("charset",  po::value<std::string>(), "the set of characters the lines can draw from when being generated (default = abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ1234567890!@#$%^&*()")
     ;
     po::variables_map options;
     po::store(po::parse_command_line(argc, argv, desc), options);
@@ -32,21 +35,27 @@ int main(int argc, char** argv){
     // parse each option, or set it to its default if not provided
     int sizeMin = (options.count("minlen")) ? options["minlen"].as<int>() : 10; 
     int sizeMax = (options.count("maxlen")) ? options["maxlen"].as<int>() : 15; 
+    int delayMs = (options.count("delay")) ? options["delay"].as<int>() : 30;
+    std::string charset = (options.count("charset")) ? options["charset"].as<std::string>() : "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+    std::string color = COLOR_GREEN;
+    if (options.count("color")){
+        try{
+            color = parseColor(options["color"].as<std::string>());
+        }
+        catch (std::invalid_argument e){
+            std::cerr << colorize("error: ", COLOR_RED) << "invalid color. Use --help to see valid color options";
+        }
+    }
     // validate the options 
     if (sizeMin >= sizeMax){
         std::cerr << colorize("error: ", COLOR_RED) << "The minimum line length must be less than the maximum" << std::endl;
         return 1;
     }
-    // in future versions, these will be user-set
-    std::string color = COLOR_GREEN;
-    std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
-    int delayMs = 30;
     std::srand(time(nullptr));
     // display the splash text in a random color
     std::vector<std::string> splashColors = {COLOR_RED, COLOR_GREEN, COLOR_YELLOW, COLOR_BLUE, COLOR_CYAN, COLOR_MAGENTA, COLOR_WHITE, COLOR_GREY};
     std::string splashColor = splashColors[std::rand() % 8];
     std::cout << colorize("Wake up, neo", splashColor) << std::endl;
-  
     // initialize the window and the line array
     TermWindow win;
     std::vector<Line> lines;
